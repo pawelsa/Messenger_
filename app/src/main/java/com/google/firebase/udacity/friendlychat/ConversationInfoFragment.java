3367b8694aa0db92a1d1fb3baa2f0509ebc5cf78
Pawel <pawel.sapek97@gmail.com>
2018-04-20 14:19:36 +0200

    Online message works fine. Added swipe back from conversation info. Working on swipe right on recyclerView with conversations to start one.

--- app/src/main/java/com/google/firebase/udacity/friendlychat/ConversationInfoFragment.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/ConversationInfoFragment.java
@@ -1,10 +1,6 @@
 package com.google.firebase.udacity.friendlychat;
 
-<<<<<<< HEAD
-=======
-import android.app.Fragment;
-import android.app.FragmentManager;
->>>>>>> github/1.00-starting-point
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -14,43 +10,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-<<<<<<< HEAD
 import me.yokeyword.swipebackfragment.SwipeBackFragment;
 
-=======
->>>>>>> github/1.00-starting-point
 /**
  * Created by Paweł on 17.04.2018.
  */
 
-<<<<<<< HEAD
 public class ConversationInfoFragment extends SwipeBackFragment {
-=======
-public class ConversationInfoFragment extends Fragment {
->>>>>>> github/1.00-starting-point
 	
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-<<<<<<< HEAD
 
 		setHasOptionsMenu(true);
-=======
-		
-		setHasOptionsMenu(true);
-		
->>>>>>> github/1.00-starting-point
 	}
 	
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-<<<<<<< HEAD
 		View view = inflater.inflate(R.layout.conversation_info, container, false);
 		return attachToSwipeBack(view);
-=======
-		return inflater.inflate(R.layout.conversation_info, container, false);
->>>>>>> github/1.00-starting-point
 	}
 	
 	@Override
@@ -64,11 +43,7 @@ public class ConversationInfoFragment extends Fragment {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case android.R.id.home:
-<<<<<<< HEAD
 				android.support.v4.app.FragmentManager fm = getFragmentManager();
-=======
-				FragmentManager fm = getFragmentManager();
->>>>>>> github/1.00-starting-point
 				if (fm.getBackStackEntryCount() > 0) {
 					fm.popBackStack();
 				}
