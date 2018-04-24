f7ecd75fab9f1b182bab8685508243dd40fa0513
Paweł Sapek <pawel.sapek97@gmail.com>
2018-04-24 23:24:54 +0200

    Added network check receiver. No idea if works. Need to add show layout when no internet connection

---
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/NetworkCheck/NetworkCheckReceiver.java
@@ -0,0 +1,25 @@
+package com.google.firebase.udacity.friendlychat.NetworkCheck;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkCheckReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        boolean isInternetConnection = isOnline(context);
+        ObserveInternet.getInstance().notifyActivity(isInternetConnection);
+    }
+
+    public boolean isOnline(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        //should check null because in airplane mode it will be null
+        return (netInfo != null && netInfo.isConnected());
+    }
+}
